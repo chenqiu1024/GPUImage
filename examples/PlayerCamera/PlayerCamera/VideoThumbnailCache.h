8e6b21331dd8bffc01e0d07871ed032694125e64
chenqiu1024 <godspeed1024@gmail.com>
2018-05-27 18:14:37 +0800

    Add missing files;

---
+++ examples/PlayerCamera/PlayerCamera/VideoThumbnailCache.h
@@ -0,0 +1,13 @@
+//
+//  VideoThumbnailCache.h
+//  PlayerCamera
+//
+//  Created by DOM QIU on 2018/5/25.
+//  Copyright © 2018 DOM QIU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoThumbnailCache : NSObject
+
+@end
---
+++ examples/PlayerCamera/PlayerCamera/VideoThumbnailCache.m
@@ -0,0 +1,13 @@
+//
+//  VideoThumbnailCache.m
+//  PlayerCamera
+//
+//  Created by DOM QIU on 2018/5/25.
+//  Copyright © 2018 DOM QIU. All rights reserved.
+//
+
+#import "VideoThumbnailCache.h"
+
+@implementation VideoThumbnailCache
+
+@end
