9cb1d2f3abfa34bbec15734c83d246ff927d6f9a
chenqiu1024 <godspeed1024@gmail.com>
2018-06-22 15:39:35 +0800

    DoctorLog; CaptureViewController for CartoonShow;

---
+++ examples/CartoonShow/CartoonShow/CaptureViewController.m
@@ -0,0 +1,226 @@
+//
+//  CaptureViewController.m
+//  CartoonShow
+//
+//  Created by DOM QIU on 2018/6/20.
+//  Copyright © 2018 Cyllenge. All rights reserved.
+//
+
+#import "CaptureViewController.h"
+#import "GPUImage.h"
+#import "GPUImageView.h"
+#import "IJKGPUImageMovie.h"
+#import <AssetsLibrary/ALAssetsLibrary.h>
+
+@interface CaptureViewController ()
+{
+    GPUImageVideoCamera* _videoCamera;
+    GPUImageOutput<GPUImageInput>* _filter;
+    
+//    IJKGPUImageMovie* _ijkMovie;
+    
+    GPUImageView* _filterView;
+    GPUImageMovieWriter* _movieWriter;
+}
+
+@end
+
+@implementation CaptureViewController
+
+#pragma mark    Movie Writer
+-(void) stopAndReleaseMovieWriter {
+    if (!_movieWriter)
+        return;
+    
+    [_movieWriter finishRecording];
+    _movieWriter = nil;
+    /*
+     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+     if ([library videoAtPathIsCompatibleWithSavedPhotosAlbum:movieURL])
+     {
+     [library writeVideoAtPathToSavedPhotosAlbum:movieURL completionBlock:^(NSURL *assetURL, NSError *error)
+     {
+     dispatch_async(dispatch_get_main_queue(), ^{
+     
+     if (error) {
+     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+     delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+     [alert show];
+     } else {
+     //                             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+     //                                                                            delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+     //                             [alert show];
+     }
+     remove(pathToMovie.UTF8String);
+     });
+     [self startRecordingVideoSegment];
+     }];
+     }
+     //*/
+}
+
+-(void) disassembleMovieWriter {
+    [_videoCamera removeTarget:_movieWriter];
+    [_videoCamera removeTarget:_filter];
+    _videoCamera.audioEncodingTarget = nil;
+    [self stopAndReleaseMovieWriter];
+}
+
+-(void) initMovieWriterWithDateTime:(NSDate*)dateTime size:(CGSize)size {
+    if (_movieWriter)
+    {
+        [self stopAndReleaseMovieWriter];
+    }
+    
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyyMMdd_hhmmss";
+    NSString* fileName = [NSString stringWithFormat:@"MOV_%@.mp4", [formatter stringFromDate:dateTime]];
+    NSString* videoDirectoryPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:VideoDirectory];
+    BOOL isDirectory = YES;
+    if (![[NSFileManager defaultManager] fileExistsAtPath:videoDirectoryPath isDirectory:&isDirectory] || !isDirectory)
+    {
+        [[NSFileManager defaultManager] createDirectoryAtPath:videoDirectoryPath withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    NSString* pathToMovie = [videoDirectoryPath stringByAppendingPathComponent:fileName];
+    //NSString* pathToMovie = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:fileName];
+    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+    NSURL* movieURL = [NSURL fileURLWithPath:pathToMovie];
+    _movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:size];
+    _movieWriter.encodingLiveVideo = YES;
+}
+
+-(void) setupMovieWriter {
+    [self initMovieWriterWithDateTime:[NSDate date] size:CGSizeMake(480.0, 640.0)];
+    [_videoCamera addTarget:_movieWriter];
+    [_videoCamera addTarget:_filter];
+    _videoCamera.audioEncodingTarget = _movieWriter;
+    ///[_ijkMovie addTarget:_movieWriter];
+}
+
+-(void) startMovieWriteRecording {
+    [_movieWriter startRecording];
+}
+
+#pragma mark - View lifecycle
+
+-(void) applicationDidBecomeActive:(id)sender {
+    [self setupMovieWriter];
+    [self startMovieWriteRecording];
+    [_videoCamera resumeCameraCapture];
+}
+
+-(void) applicationWillResignActive:(id)sender {
+    [self disassembleMovieWriter];
+    [_videoCamera pauseCameraCapture];
+}
+
+-(void) dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+///    [self removeMovieNotificationObservers];
+}
+
+- (BOOL) prefersStatusBarHidden {
+    return NO;
+}
+
+-(UIStatusBarStyle) preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSNotificationCenter* nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(applicationWillResignActive:) name:UIApplicationWillResignActiveNotification object:nil];
+    [nc addObserver:self selector:@selector(applicationDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    
+    //    self.navigationController.navigationBarHidden = YES;
+    
+    _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    _videoCamera.horizontallyMirrorFrontFacingCamera = NO;
+    _videoCamera.horizontallyMirrorRearFacingCamera = NO;
+    
+    _filterView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+    _filterView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:_filterView];
+    _filterView.fillMode = kGPUImageFillModePreserveAspectRatio;
+    /*///
+    [self.view bringSubviewToFront:self.overlayView];
+    [self.view bringSubviewToFront:self.controlPanelView];
+    
+    UIPanGestureRecognizer* panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPanRecognized:)];
+    panRecognizer.minimumNumberOfTouches = 1;
+    [self.overlayView addGestureRecognizer:panRecognizer];
+    _fastSeekStartTime = 0.f;
+    
+    _isProgressSliderBeingDragged = NO;
+    self.playOrPauseButton.tag = 100;
+    //*/
+    _filter = [[GPUImageSepiaFilter alloc] init];
+    [(GPUImageSepiaFilter*)_filter setIntensity:1.f];
+    [_filter addTarget:_filterView];
+    
+/*///    [self installMovieNotificationObservers];
+#ifdef SourceVideoFileName
+    _ijkMovie = [[IJKGPUImageMovie alloc] initWithContentURLString:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:SourceVideoFileName]];
+#else
+    _ijkMovie = [[IJKGPUImageMovie alloc] initWithContentURLString:self.sourceVideoFile];
+#endif
+    _ijkMovie.delegate = self;
+    [_ijkMovie setPauseInBackground:YES];
+    [_ijkMovie addTarget:_filter];
+    [_ijkMovie prepareToPlay];
+    [self refreshMediaControl];
+//*/
+    [self setupMovieWriter];
+    [self startMovieWriteRecording];
+    [_videoCamera startCameraCapture];
+    [_videoCamera resumeCameraCapture];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    // Map UIDeviceOrientation to UIInterfaceOrientation.
+    UIInterfaceOrientation orient = UIInterfaceOrientationPortrait;
+    switch ([[UIDevice currentDevice] orientation])
+    {
+        case UIDeviceOrientationLandscapeLeft:
+            orient = UIInterfaceOrientationLandscapeLeft;
+            break;
+            
+        case UIDeviceOrientationLandscapeRight:
+            orient = UIInterfaceOrientationLandscapeRight;
+            break;
+            
+        case UIDeviceOrientationPortrait:
+            orient = UIInterfaceOrientationPortrait;
+            break;
+            
+        case UIDeviceOrientationPortraitUpsideDown:
+            orient = UIInterfaceOrientationPortraitUpsideDown;
+            break;
+            
+        case UIDeviceOrientationFaceUp:
+        case UIDeviceOrientationFaceDown:
+        case UIDeviceOrientationUnknown:
+            // When in doubt, stay the same.
+            orient = fromInterfaceOrientation;
+            break;
+    }
+    _videoCamera.outputImageOrientation = orient;
+    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES; // Support all orientations.
+}
+
+@end
