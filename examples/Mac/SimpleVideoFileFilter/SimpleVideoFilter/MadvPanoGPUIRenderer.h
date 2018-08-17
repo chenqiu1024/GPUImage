d002cf1f0b9fc40b6db73087838db08809bb950e
chenqiu1024 <godspeed1024@gmail.com>
2018-08-17 17:13:42 +0800

    Simplified;

--- examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/MadvPanoGPUIRenderer.h
+++ examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/MadvPanoGPUIRenderer.h
@@ -8,17 +8,7 @@
 {
     GPUImageFramebuffer *firstInputFramebuffer;
     
-    GLProgram *filterProgram;
-    GLint filterPositionAttribute, filterTextureCoordinateAttribute;
-    GLint filterInputTextureUniform;
-    GLfloat backgroundColorRed, backgroundColorGreen, backgroundColorBlue, backgroundColorAlpha;
-    
     BOOL isEndProcessing;
-    
-    CGSize currentFilterSize;
-    GPUImageRotationMode inputRotation;
-    
-    BOOL currentlyReceivingMonochromeInput;
     
     NSMutableDictionary *uniformStateRestorationBlocks;
     dispatch_semaphore_t imageCaptureSemaphore;
@@ -26,35 +16,10 @@
 
 @property(readonly) CVPixelBufferRef renderTarget;///!!!
 @property(readwrite, nonatomic) BOOL preventRendering;
-@property(readwrite, nonatomic) BOOL currentlyReceivingMonochromeInput;
 
 /// @name Initialization and teardown
 
-/**
- Initialize with vertex and fragment shaders
- 
- You make take advantage of the SHADER_STRING macro to write your shaders in-line.
- @param vertexShaderString Source code of the vertex shader to use
- @param fragmentShaderString Source code of the fragment shader to use
- */
-- (id)initWithVertexShaderFromString:(NSString *)vertexShaderString fragmentShaderFromString:(NSString *)fragmentShaderString;
-
-/**
- Initialize with a fragment shader
- 
- You may take advantage of the SHADER_STRING macro to write your shader in-line.
- @param fragmentShaderString Source code of fragment shader to use
- */
-- (id)initWithFragmentShaderFromString:(NSString *)fragmentShaderString;
-/**
- Initialize with a fragment shader
- @param fragmentShaderFilename Filename of fragment shader to load
- */
-- (id)initWithFragmentShaderFromFile:(NSString *)fragmentShaderFilename;
-- (void)initializeAttributes;
-- (void)setupFilterForSize:(CGSize)filterFrameSize;
-- (CGSize)rotatedSize:(CGSize)sizeToRotate forIndex:(NSInteger)textureIndex;
-- (CGPoint)rotatedPoint:(CGPoint)pointToRotate forRotation:(GPUImageRotationMode)rotation;
+- (id)init;
 
 /// @name Managing the display FBOs
 /** Size of the frame buffer object
@@ -62,32 +27,7 @@
 - (CGSize)sizeOfFBO;
 
 /// @name Rendering
-+ (const GLfloat *)textureCoordinatesForRotation:(GPUImageRotationMode)rotationMode;
-- (void)renderToTextureWithVertices:(const GLfloat *)vertices textureCoordinates:(const GLfloat *)textureCoordinates;
 - (void)informTargetsAboutNewFrameAtTime:(CMTime)frameTime;
 - (CGSize)outputFrameSize;
-
-/// @name Input parameters
-- (void)setBackgroundColorRed:(GLfloat)redComponent green:(GLfloat)greenComponent blue:(GLfloat)blueComponent alpha:(GLfloat)alphaComponent;
-- (void)setInteger:(GLint)newInteger forUniformName:(NSString *)uniformName;
-- (void)setFloat:(GLfloat)newFloat forUniformName:(NSString *)uniformName;
-- (void)setSize:(CGSize)newSize forUniformName:(NSString *)uniformName;
-- (void)setPoint:(CGPoint)newPoint forUniformName:(NSString *)uniformName;
-- (void)setFloatVec3:(GPUVector3)newVec3 forUniformName:(NSString *)uniformName;
-- (void)setFloatVec4:(GPUVector4)newVec4 forUniform:(NSString *)uniformName;
-- (void)setFloatArray:(GLfloat *)array length:(GLsizei)count forUniform:(NSString*)uniformName;
-
-- (void)setMatrix3f:(GPUMatrix3x3)matrix forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setMatrix4f:(GPUMatrix4x4)matrix forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setFloat:(GLfloat)floatValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setPoint:(CGPoint)pointValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setSize:(CGSize)sizeValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setVec3:(GPUVector3)vectorValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setVec4:(GPUVector4)vectorValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setFloatArray:(GLfloat *)arrayValue length:(GLsizei)arrayLength forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-- (void)setInteger:(GLint)intValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-
-- (void)setAndExecuteUniformStateCallbackAtIndex:(GLint)uniform forProgram:(GLProgram *)shaderProgram toBlock:(dispatch_block_t)uniformStateBlock;
-- (void)setUniformsForProgramAtIndex:(NSUInteger)programIndex;
 
 @end
--- examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/MadvPanoGPUIRenderer.mm
+++ examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/MadvPanoGPUIRenderer.mm
@@ -6,108 +6,26 @@
 @implementation MadvPanoGPUIRenderer
 
 @synthesize preventRendering = _preventRendering;
-@synthesize currentlyReceivingMonochromeInput;
 
 #pragma mark -
 #pragma mark Initialization and teardown
 
-- (id)initWithVertexShaderFromString:(NSString *)vertexShaderString fragmentShaderFromString:(NSString *)fragmentShaderString;
+- (id)init
 {
     if (!(self = [super init]))
     {
         return nil;
     }
     
-    uniformStateRestorationBlocks = [NSMutableDictionary dictionaryWithCapacity:10];
     _preventRendering = NO;
-    currentlyReceivingMonochromeInput = NO;
-    inputRotation = kGPUImageNoRotation;
-    backgroundColorRed = 0.0;
-    backgroundColorGreen = 0.0;
-    backgroundColorBlue = 0.0;
-    backgroundColorAlpha = 0.0;
     imageCaptureSemaphore = dispatch_semaphore_create(0);
     dispatch_semaphore_signal(imageCaptureSemaphore);
     
     runSynchronouslyOnVideoProcessingQueue(^{
         [GPUImageContext useImageProcessingContext];
-        
-        filterProgram = [[GPUImageContext sharedImageProcessingContext] programForVertexShaderString:vertexShaderString fragmentShaderString:fragmentShaderString];
-        
-        if (!filterProgram.initialized)
-        {
-            [self initializeAttributes];
-            
-            if (![filterProgram link])
-            {
-                NSString *progLog = [filterProgram programLog];
-                NSLog(@"Program link log: %@", progLog);
-                NSString *fragLog = [filterProgram fragmentShaderLog];
-                NSLog(@"Fragment shader compile log: %@", fragLog);
-                NSString *vertLog = [filterProgram vertexShaderLog];
-                NSLog(@"Vertex shader compile log: %@", vertLog);
-                filterProgram = nil;
-                NSAssert(NO, @"Filter shader link failed");
-            }
-        }
-        
-        filterPositionAttribute = [filterProgram attributeIndex:@"position"];
-        filterTextureCoordinateAttribute = [filterProgram attributeIndex:@"inputTextureCoordinate"];
-        filterInputTextureUniform = [filterProgram uniformIndex:@"inputImageTexture"]; // This does assume a name of "inputImageTexture" for the fragment shader
-        
-        [GPUImageContext setActiveShaderProgram:filterProgram];
-        
-        glEnableVertexAttribArray(filterPositionAttribute);
-        glEnableVertexAttribArray(filterTextureCoordinateAttribute);
     });
     
     return self;
-}
-
-- (id)initWithFragmentShaderFromString:(NSString *)fragmentShaderString;
-{
-    if (!(self = [self initWithVertexShaderFromString:kGPUImageVertexShaderString fragmentShaderFromString:fragmentShaderString]))
-    {
-        return nil;
-    }
-    
-    return self;
-}
-
-- (id)initWithFragmentShaderFromFile:(NSString *)fragmentShaderFilename;
-{
-    NSString *fragmentShaderPathname = [[NSBundle mainBundle] pathForResource:fragmentShaderFilename ofType:@"fsh"];
-    NSString *fragmentShaderString = [NSString stringWithContentsOfFile:fragmentShaderPathname encoding:NSUTF8StringEncoding error:nil];
-    
-    if (!(self = [self initWithFragmentShaderFromString:fragmentShaderString]))
-    {
-        return nil;
-    }
-    
-    return self;
-}
-
-- (id)init;
-{
-    if (!(self = [self initWithFragmentShaderFromString:kGPUImagePassthroughFragmentShaderString]))
-    {
-        return nil;
-    }
-    
-    return self;
-}
-
-- (void)initializeAttributes;
-{
-    [filterProgram addAttribute:@"position"];
-    [filterProgram addAttribute:@"inputTextureCoordinate"];
-    
-    // Override this, calling back to this super method, in order to add new attributes to your vertex shader
-}
-
-- (void)setupFilterForSize:(CGSize)filterFrameSize;
-{
-    // This is where you can override to provide some custom setup, if your filter has a size-dependent element
 }
 
 - (void)dealloc
@@ -174,86 +92,13 @@
 #pragma mark -
 #pragma mark Rendering
 
-+ (const GLfloat *)textureCoordinatesForRotation:(GPUImageRotationMode)rotationMode;
-{
-    static const GLfloat noRotationTextureCoordinates[] = {
-        0.0f, 0.0f,
-        1.0f, 0.0f,
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-    };
-    
-    static const GLfloat rotateLeftTextureCoordinates[] = {
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-        0.0f, 0.0f,
-        0.0f, 1.0f,
-    };
-    
-    static const GLfloat rotateRightTextureCoordinates[] = {
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-        1.0f, 1.0f,
-        1.0f, 0.0f,
-    };
-    
-    static const GLfloat verticalFlipTextureCoordinates[] = {
-        0.0f, 1.0f,
-        1.0f, 1.0f,
-        0.0f,  0.0f,
-        1.0f,  0.0f,
-    };
-    
-    static const GLfloat horizontalFlipTextureCoordinates[] = {
-        1.0f, 0.0f,
-        0.0f, 0.0f,
-        1.0f,  1.0f,
-        0.0f,  1.0f,
-    };
-    
-    static const GLfloat rotateRightVerticalFlipTextureCoordinates[] = {
-        0.0f, 0.0f,
-        0.0f, 1.0f,
-        1.0f, 0.0f,
-        1.0f, 1.0f,
-    };
-    
-    static const GLfloat rotateRightHorizontalFlipTextureCoordinates[] = {
-        1.0f, 1.0f,
-        1.0f, 0.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-    };
-    
-    static const GLfloat rotate180TextureCoordinates[] = {
-        1.0f, 1.0f,
-        0.0f, 1.0f,
-        1.0f, 0.0f,
-        0.0f, 0.0f,
-    };
-    
-    switch(rotationMode)
-    {
-        case kGPUImageNoRotation: return noRotationTextureCoordinates;
-        case kGPUImageRotateLeft: return rotateLeftTextureCoordinates;
-        case kGPUImageRotateRight: return rotateRightTextureCoordinates;
-        case kGPUImageFlipVertical: return verticalFlipTextureCoordinates;
-        case kGPUImageFlipHorizonal: return horizontalFlipTextureCoordinates;
-        case kGPUImageRotateRightFlipVertical: return rotateRightVerticalFlipTextureCoordinates;
-        case kGPUImageRotateRightFlipHorizontal: return rotateRightHorizontalFlipTextureCoordinates;
-        case kGPUImageRotate180: return rotate180TextureCoordinates;
-    }
-}
-
-- (void)renderToTextureWithVertices:(const GLfloat *)vertices textureCoordinates:(const GLfloat *)textureCoordinates;
+- (void)render
 {
     if (self.preventRendering)
     {
         [firstInputFramebuffer unlock];
         return;
     }
-    
-    [GPUImageContext setActiveShaderProgram:filterProgram];
     
     outputFramebuffer = [[GPUImageContext sharedFramebufferCache] fetchFramebufferForSize:[self sizeOfFBO] textureOptions:self.outputTextureOptions onlyTexture:NO];
     [outputFramebuffer activateFramebuffer];
@@ -262,20 +107,13 @@
         [outputFramebuffer lock];
     }
     
-    [self setUniformsForProgramAtIndex:0];
-    
-    glClearColor(backgroundColorRed, backgroundColorGreen, backgroundColorBlue, backgroundColorAlpha);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
     
     glActiveTexture(GL_TEXTURE2);
     glBindTexture(GL_TEXTURE_2D, [firstInputFramebuffer texture]);
     
-    glUniform1i(filterInputTextureUniform, 2);
-    
-    glVertexAttribPointer(filterPositionAttribute, 2, GL_FLOAT, 0, 0, vertices);
-    glVertexAttribPointer(filterTextureCoordinateAttribute, 2, GL_FLOAT, 0, 0, textureCoordinates);
-    
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    //TODO:
     
     [firstInputFramebuffer unlock];
     
@@ -337,190 +175,12 @@
 #pragma mark -
 #pragma mark Input parameters
 
-- (void)setBackgroundColorRed:(GLfloat)redComponent green:(GLfloat)greenComponent blue:(GLfloat)blueComponent alpha:(GLfloat)alphaComponent;
-{
-    backgroundColorRed = redComponent;
-    backgroundColorGreen = greenComponent;
-    backgroundColorBlue = blueComponent;
-    backgroundColorAlpha = alphaComponent;
-}
-
-- (void)setInteger:(GLint)newInteger forUniformName:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setInteger:newInteger forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setFloat:(GLfloat)newFloat forUniformName:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setFloat:newFloat forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setSize:(CGSize)newSize forUniformName:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setSize:newSize forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setPoint:(CGPoint)newPoint forUniformName:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setPoint:newPoint forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setFloatVec3:(GPUVector3)newVec3 forUniformName:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setVec3:newVec3 forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setFloatVec4:(GPUVector4)newVec4 forUniform:(NSString *)uniformName;
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    [self setVec4:newVec4 forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setFloatArray:(GLfloat *)array length:(GLsizei)count forUniform:(NSString*)uniformName
-{
-    GLint uniformIndex = [filterProgram uniformIndex:uniformName];
-    
-    [self setFloatArray:array length:count forUniform:uniformIndex program:filterProgram];
-}
-
-- (void)setMatrix3f:(GPUMatrix3x3)matrix forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniformMatrix3fv(uniform, 1, GL_FALSE, (GLfloat *)&matrix);
-        }];
-    });
-}
-
-- (void)setMatrix4f:(GPUMatrix4x4)matrix forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniformMatrix4fv(uniform, 1, GL_FALSE, (GLfloat *)&matrix);
-        }];
-    });
-}
-
-- (void)setFloat:(GLfloat)floatValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniform1f(uniform, floatValue);
-        }];
-    });
-}
-
-- (void)setPoint:(CGPoint)pointValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            GLfloat positionArray[2];
-            positionArray[0] = pointValue.x;
-            positionArray[1] = pointValue.y;
-            
-            glUniform2fv(uniform, 1, positionArray);
-        }];
-    });
-}
-
-- (void)setSize:(CGSize)sizeValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            GLfloat sizeArray[2];
-            sizeArray[0] = sizeValue.width;
-            sizeArray[1] = sizeValue.height;
-            
-            glUniform2fv(uniform, 1, sizeArray);
-        }];
-    });
-}
-
-- (void)setVec3:(GPUVector3)vectorValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniform3fv(uniform, 1, (GLfloat *)&vectorValue);
-        }];
-    });
-}
-
-- (void)setVec4:(GPUVector4)vectorValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniform4fv(uniform, 1, (GLfloat *)&vectorValue);
-        }];
-    });
-}
-
-- (void)setFloatArray:(GLfloat *)arrayValue length:(GLsizei)arrayLength forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    // Make a copy of the data, so it doesn't get overwritten before async call executes
-    NSData* arrayData = [NSData dataWithBytes:arrayValue length:arrayLength * sizeof(arrayValue[0])];
-    
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniform1fv(uniform, arrayLength, (GLfloat*)[arrayData bytes]);
-        }];
-    });
-}
-
-- (void)setInteger:(GLint)intValue forUniform:(GLint)uniform program:(GLProgram *)shaderProgram;
-{
-    runAsynchronouslyOnVideoProcessingQueue(^{
-        [GPUImageContext setActiveShaderProgram:shaderProgram];
-        
-        [self setAndExecuteUniformStateCallbackAtIndex:uniform forProgram:shaderProgram toBlock:^{
-            glUniform1i(uniform, intValue);
-        }];
-    });
-}
-
-- (void)setAndExecuteUniformStateCallbackAtIndex:(GLint)uniform forProgram:(GLProgram *)shaderProgram toBlock:(dispatch_block_t)uniformStateBlock;
-{
-    [uniformStateRestorationBlocks setObject:[uniformStateBlock copy] forKey:[NSNumber numberWithInt:uniform]];
-    uniformStateBlock();
-}
-
-- (void)setUniformsForProgramAtIndex:(NSUInteger)programIndex;
-{
-    [uniformStateRestorationBlocks enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop){
-        dispatch_block_t currentBlock = obj;
-        currentBlock();
-    }];
-}
-
 #pragma mark -
 #pragma mark GPUImageInput
 
 - (void)newFrameReadyAtTime:(CMTime)frameTime atIndex:(NSInteger)textureIndex;
 {
-    static const GLfloat imageVertices[] = {
-        -1.0f, -1.0f,
-        1.0f, -1.0f,
-        -1.0f,  1.0f,
-        1.0f,  1.0f,
-    };
-    
-    [self renderToTextureWithVertices:imageVertices textureCoordinates:[[self class] textureCoordinatesForRotation:inputRotation]];
+    [self render];
     
     [self informTargetsAboutNewFrameAtTime:frameTime];
 }
@@ -534,105 +194,6 @@
 {
     firstInputFramebuffer = newInputFramebuffer;
     [firstInputFramebuffer lock];
-}
-
-- (CGSize)rotatedSize:(CGSize)sizeToRotate forIndex:(NSInteger)textureIndex;
-{
-    CGSize rotatedSize = sizeToRotate;
-    
-    if (GPUImageRotationSwapsWidthAndHeight(inputRotation))
-    {
-        rotatedSize.width = sizeToRotate.height;
-        rotatedSize.height = sizeToRotate.width;
-    }
-    
-    return rotatedSize;
-}
-
-- (CGPoint)rotatedPoint:(CGPoint)pointToRotate forRotation:(GPUImageRotationMode)rotation;
-{
-    CGPoint rotatedPoint;
-    switch(rotation)
-    {
-        case kGPUImageNoRotation: return pointToRotate; break;
-        case kGPUImageFlipHorizonal:
-        {
-            rotatedPoint.x = 1.0 - pointToRotate.x;
-            rotatedPoint.y = pointToRotate.y;
-        }; break;
-        case kGPUImageFlipVertical:
-        {
-            rotatedPoint.x = pointToRotate.x;
-            rotatedPoint.y = 1.0 - pointToRotate.y;
-        }; break;
-        case kGPUImageRotateLeft:
-        {
-            rotatedPoint.x = 1.0 - pointToRotate.y;
-            rotatedPoint.y = pointToRotate.x;
-        }; break;
-        case kGPUImageRotateRight:
-        {
-            rotatedPoint.x = pointToRotate.y;
-            rotatedPoint.y = 1.0 - pointToRotate.x;
-        }; break;
-        case kGPUImageRotateRightFlipVertical:
-        {
-            rotatedPoint.x = pointToRotate.y;
-            rotatedPoint.y = pointToRotate.x;
-        }; break;
-        case kGPUImageRotateRightFlipHorizontal:
-        {
-            rotatedPoint.x = 1.0 - pointToRotate.y;
-            rotatedPoint.y = 1.0 - pointToRotate.x;
-        }; break;
-        case kGPUImageRotate180:
-        {
-            rotatedPoint.x = 1.0 - pointToRotate.x;
-            rotatedPoint.y = 1.0 - pointToRotate.y;
-        }; break;
-    }
-    
-    return rotatedPoint;
-}
-
-- (void)setInputSize:(CGSize)newSize atIndex:(NSInteger)textureIndex;
-{
-    if (self.preventRendering)
-    {
-        return;
-    }
-    
-    if (overrideInputSize)
-    {
-        if (CGSizeEqualToSize(forcedMaximumSize, CGSizeZero))
-        {
-        }
-        else
-        {
-            CGRect insetRect = AVMakeRectWithAspectRatioInsideRect(newSize, CGRectMake(0.0, 0.0, forcedMaximumSize.width, forcedMaximumSize.height));
-            inputTextureSize = insetRect.size;
-        }
-    }
-    else
-    {
-        CGSize rotatedSize = [self rotatedSize:newSize forIndex:textureIndex];
-        
-        if (CGSizeEqualToSize(rotatedSize, CGSizeZero))
-        {
-            inputTextureSize = rotatedSize;
-        }
-        else if (!CGSizeEqualToSize(inputTextureSize, rotatedSize))
-        {
-            inputTextureSize = rotatedSize;
-        }
-    }
-    
-    [self setupFilterForSize:[self sizeOfFBO]];
-}
-
-- (void)setInputRotation:(GPUImageRotationMode)newInputRotation atIndex:(NSInteger)textureIndex;
-{
-    inputRotation = newInputRotation;
 }
 
 - (void)forceProcessingAtSize:(CGSize)frameSize;
