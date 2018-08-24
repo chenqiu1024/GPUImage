55612321316c3b3cff13cbbe95b417329b7879ff
qiudong <dong.qiu@madv360.com>
2018-08-24 18:10:46 +0800

    Some UI outlets;

--- examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/AppDelegate.m
+++ examples/Mac/SimpleVideoFileFilter/SimpleVideoFilter/AppDelegate.m
@@ -4,14 +4,23 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet NSCollectionView* collectionView;
+
+-(IBAction)openFiles:(id)sender;
+
 @end
 
 @implementation AppDelegate
 
+-(IBAction)openFiles:(id)sender {
+    NSLog(@"Open Files");
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    simpleVideoFileFilterWindowController = [[SLSSimpleVideoFileFilterWindowController alloc] initWithWindowNibName:@"SLSSimpleVideoFileFilterWindowController"];
-    [simpleVideoFileFilterWindowController showWindow:self];
+//    simpleVideoFileFilterWindowController = [[SLSSimpleVideoFileFilterWindowController alloc] initWithWindowNibName:@"SLSSimpleVideoFileFilterWindowController"];
+//    [simpleVideoFileFilterWindowController showWindow:self];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
