2d73d786e8cc5971bc87bb466c0c94f06fcb5d13
chenqiu1024 <godspeed1024@gmail.com>
2019-05-03 09:55:31 +0800

    Import

---
+++ framework/Source/GPUImageOverlapping2InputFilter.h
@@ -0,0 +1,17 @@
+//
+//  GPUImageOverlapping2InputFilter.h
+//  GPUImage
+//
+//  Created by DOM QIU on 2019/5/3.
+//  Copyright © 2019 Brad Larson. All rights reserved.
+//
+
+#import "GPUImageTwoInputFilter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GPUImageOverlapping2InputFilter : GPUImageTwoInputFilter
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ framework/Source/GPUImageOverlapping2InputFilter.m
@@ -0,0 +1,13 @@
+//
+//  GPUImageOverlapping2InputFilter.m
+//  GPUImage
+//
+//  Created by DOM QIU on 2019/5/3.
+//  Copyright © 2019 Brad Larson. All rights reserved.
+//
+
+#import "GPUImageOverlapping2InputFilter.h"
+
+@implementation GPUImageOverlapping2InputFilter
+
+@end
